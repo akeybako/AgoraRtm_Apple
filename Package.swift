113e0e74e64c9964c6d1ac1cc2ee6ce443230ab7
Ohagi <akito.hagio@voicy.jp>
2025-11-19 20:15:14 +0900

    Update AgoraRtmKit checksum in Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "AgoraRtmKit",
             url: "https://download.agora.io/rtmsdk/release/AgoraRtmKit.xcframework_2.2.5.zip",
-            checksum: "95d06dbef28ac26d1a5ddcb1fd6bf0148d54589b0c18c74fb98a20e10ed15cac"
+            checksum: "856aba19003bcfaf09daac35db6717a068a3a687d2fe99a16809d810afa069bc"
         ),
         // 注: aoslターゲットは削除し、AgoraInfra_iOSから提供されるものを使用
         // これによりAgoraRtcEngine_iOSとの競合を回避
